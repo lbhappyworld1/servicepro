78f90bf37d40f719bbda3e9356a5739d6506dfbd
lbhappyworld1 <lbhappyworld1@163.com>
2019-12-03 22:09:44 +0800

    update

--- api/controllers/classroom/view-classroom-info-release.js
+++ api/controllers/classroom/view-classroom-info-release.js
@@ -37,7 +37,7 @@ module.exports = {
       if(!classrooms.classroomqrcodeurl ||input.isrefresh=="Y"){
         var wxtokent = await axios.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxd01fdc34cb1cce99&secret=4625eb509f588c33e5f6c080a60c577b')
         // var qrcodecreateobj = await axios.post('https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=' + wxtokent.data.access_token, { "expire_seconds": 1800, "action_name": "QR_STR_SCENE", "action_info": { "scene": { "scene_str":input.classromId } } });
-        var qrcodecreateobj = await axios.post('https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=' + wxtokent.data.access_token, { "action_name": "QR_LIMIT_SCENE", "action_info": { "scene": { "scene_str":input.classromId } } });
+        var qrcodecreateobj = await axios.post('https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=' + wxtokent.data.access_token, { "action_name": "QR_LIMIT_STR_SCENE", "action_info": { "scene": { "scene_str":input.classromId } } });
         var qrcode = await axios.get(' https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=' + qrcodecreateobj.data.ticket);
         
         await ClassRoom.updateOne({ id: input.classromId })
