4f3095153228814035c4efc9c4f5b52089152779
lbhappyworld1 <lbhappyworld1@163.com>
2020-01-05 18:55:19 +0800

    update

--- api/controllers/wx/wxtoken.js
+++ api/controllers/wx/wxtoken.js
@@ -155,7 +155,7 @@ requests over WebSockets instead of HTTP).`,
            //获取用户信息https://api.weixin.qq.com/cgi-bin/user/info?access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN
            var wxtokent = await axios.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxd01fdc34cb1cce99&secret=4625eb509f588c33e5f6c080a60c577b')
            sails.log.info('userinfo:' + fuser+" "+wxtokent.access_token+" "+touser);
-           var userinfo = await axios.get('https://api.weixin.qq.com/cgi-bin/user/info?access_token='+wxtokent.access_token+'&openid='+fuser+'&lang=zh_CN')
+           var userinfo = await axios.get('https://api.weixin.qq.com/cgi-bin/user/info?access_token='+wxtokent.data.access_token+'&openid='+fuser+'&lang=zh_CN')
            sails.log.info('userinfo:' + userinfo.toString());
            var userisin = await SigninUser.find(
              {
