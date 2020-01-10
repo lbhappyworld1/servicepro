bd91b2333b4f4f02a3b35be101604d3326da44d3
liubin <103818443@qq.com>
2020-01-10 18:04:17 +0800

    update

--- config/local.js
+++ config/local.js
@@ -12,6 +12,6 @@ module.exports = {
 
   // Any configuration settings may be overridden below, whether it's built-in Sails
   // options or custom configuration specifically for your app (e.g. Stripe, Mailgun, etc.)
-  port:8000
+  port:80
 
 };
